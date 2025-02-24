48fc3efd8fbc6161b8c301d2e93ac3687e3bcaf6
Lijo <lijoraju37@gmail.com>
2025-02-24 16:46:34 +0530

    changes for running in real time detection mode

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from src.data_preprocessing import FaceMaskDataset, process_annotations
 from src.model import create_vgg16bn_model
 from src.evaluate import evaluate_test_set
 from src.utils import load_config
+from src.realtime_detection import realtime_detection
 
 from sklearn.model_selection import train_test_split
 import torch
@@ -23,30 +24,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Face Mask Detection")
     parser.add_argument("--train", action="store_true", help="Train the model")
     parser.add_argument("--inference", type=str, help="Path to image for inference")
+    parser.add_argument("--realtime", action="store_true", help="Perform real-time detection")
     args = parser.parse_args()
 
-    #Load dataframe
-    df = process_annotations(ANNOTATION_PATH)
-
-    #Split the data
-    train_df, test_df = train_test_split(df, test_size=0.2, random_state=42, stratify=df['label'])
-    train_df, val_df = train_test_split(train_df, test_size=0.1, random_state=42, stratify=train_df['label'])
-
-    #Create datasets
-    train_dataset = FaceMaskDataset(train_df, IMAGE_PATH, augment=True)
-    val_dataset = FaceMaskDataset(val_df, IMAGE_PATH, augment=False)
-    test_dataset = FaceMaskDataset(test_df, IMAGE_PATH, augment=False)
-
-    #Create dataloaders
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = create_vgg16bn_model(num_classes=3, pretrained=True)  
-    model.to(device)
 
     if args.train:
+        #Load dataframe
+        df = process_annotations(ANNOTATION_PATH)
+
+        #Split the data
+        train_df, test_df = train_test_split(df, test_size=0.2, random_state=42, stratify=df['label'])
+        train_df, val_df = train_test_split(train_df, test_size=0.1, random_state=42, stratify=train_df['label'])
+
+        #Create datasets
+        train_dataset = FaceMaskDataset(train_df, IMAGE_PATH, augment=True)
+        val_dataset = FaceMaskDataset(val_df, IMAGE_PATH, augment=False)
+        test_dataset = FaceMaskDataset(test_df, IMAGE_PATH, augment=False)
+
+        #Create dataloaders
+        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+        model = create_vgg16bn_model(num_classes=3, pretrained=True)  
+        model.to(device)
+
         train_model(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001, device=device)
         print("Training finished!")
         # After training, load the best model and evaluate on the test set:
@@ -74,3 +77,5 @@ if __name__ == "__main__":
             print(f"Probability of Predicted Class: {predicted_probability:.4f}")
         else:
             print("Inference failed.")
+    elif args.realtime:
+        realtime_detection(device)
