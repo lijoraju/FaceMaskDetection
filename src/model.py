d0521de9b26a9cfb2c219c18d452b94f20e365f1
Lijo <lijoraju37@gmail.com>
2025-02-27 21:30:16 +0530

    refactor: Trained a more lightweight model -  MobileNetV3

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@
 import torch
 import torchvision.models as models
 import torch.nn as nn
-from torchvision.models import VGG16_BN_Weights
+from torchvision.models import MobileNet_V3_Small_Weights, MobileNet_V3_Large_Weights
 
 def create_resnet_model(num_classes=3, pretrained=True, resnet_version="resnet50"):  
     """Creates a ResNet model."""
@@ -22,6 +22,26 @@ def create_resnet_model(num_classes=3, pretrained=True, resnet_version="resnet50
 def load_resnet_model(model_path, num_classes=3, resnet_version="resnet50", device="cpu"):
     model = create_resnet_model(num_classes, pretrained=False, resnet_version=resnet_version)
     model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
+    model.to(device)
+    model.eval()
+    return model
+
+def create_mobilenetv3_model(num_classes=3, pretrained=True, mobilenet_version="mobilenet_v3_small"):
+    """Creates a MobileNetV3 model."""
+    if mobilenet_version == "mobilenet_v3_small":
+        model = models.mobilenet_v3_small(pretrained=pretrained, weights=MobileNet_V3_Small_Weights.IMAGENET1K_V1)
+    elif mobilenet_version == "mobilenet_v3_large":
+        model = models.mobilenet_v3_large(pretrained=pretrained, weights=MobileNet_V3_Large_Weights.IMAGENET1K_V1)
+    else:
+        raise ValueError("Invalid MobileNetV3 version. Choose from 'mobilenet_v3_small' or 'mobilenet_v3_large'.")
+
+    in_features = model.classifier[3].in_features 
+    model.classifier[3] = nn.Linear(in_features, num_classes)  
+    return model
+
+def load_mobilenetv3_model(model_path, num_classes=3, mobilenet_version="mobilenet_v3_small", device="cpu"):
+    model = create_mobilenetv3_model(num_classes, pretrained=False, mobilenet_version=mobilenet_version)
+    model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True), strict=False) 
     model.to(device)
     model.eval()
     return model
