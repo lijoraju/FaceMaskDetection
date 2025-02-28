8dd729a35fe7f7651fa46f4394213e95dee303fd
Lijo <lijoraju37@gmail.com>
2025-02-28 22:13:30 +0530

    Refactor: Updated config file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 
 from src.train import train_model  
 from src.inference import inference
-from src.data_preprocessing import FaceMaskDataset, process_annotations
+from src.data_preprocessing import FaceMaskDataset, process_annotations, download_dataset
 from src.model import create_mobilenetv3_model, load_mobilenetv3_model
 from src.evaluate import evaluate_test_set
 from src.utils import load_config
@@ -22,9 +22,13 @@ if __name__ == "__main__":
 
     config = load_config()
     if config:
-        ANNOTATION_PATH = config['annotation_path']
-        IMAGE_PATH = config['image_path']
+        DATASET = config['kaggle_dataset']
+        ANNOTATION_PATH, IMAGE_PATH = download_dataset(DATASET)
         MODEL_PATH = config['best_model_path']
+        BATCH_SIZE = config['batch_size']
+        LR = config['learning_rate']
+        NUM_OF_EPOCHS = config['num_epochs']
+        PATIENCE = config['patience']
     else:
         exit("Failed to load configuration. Exiting.")
 
@@ -44,14 +48,14 @@ if __name__ == "__main__":
         test_dataset = FaceMaskDataset(test_df, IMAGE_PATH, augment=False)
 
         #Create dataloaders
-        train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-        val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-        test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+        train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+        test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
  
         model = create_mobilenetv3_model(num_classes=3, pretrained=True)
         model.to(device)
 
-        train_model(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001, device=device)
+        train_model(model, train_loader, val_loader, num_epochs=NUM_OF_EPOCHS, learning_rate=LR, patience=PATIENCE, device=device)
         print("Training finished!")
         # After training, load the best model and evaluate on the test set:
         model = load_mobilenetv3_model(MODEL_PATH, num_classes=3, device=device)
